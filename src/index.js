2c622f2146359617aa63fea2b8263b05c82595cc
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-07-15 19:02:37 +0300

    Adds index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newTaskDescription = document.getElementById('new-task-description').value;
+    const newTask = document.createElement('li');
+    newTask.textContent = newTaskDescription;
+
+    // THis will create a delete button for tasks
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      newTask.remove();
+    });
+
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+    document.getElementById('new-task-description').value = '';
+  });
 });
